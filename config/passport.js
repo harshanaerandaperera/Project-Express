3341b22a22628a65b263695541d5d688f131c26f
Harshana Perera <harshanatxn@gmail.com>
2018-07-27 07:22:24 +0530

    cleaning code

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,3 @@
-//This module lets you authenticate endpoints using a JSON web token. It is intended to be used to secure RESTful endpoints without sessions.
-//when we send a request , we can use passport-jwt for identify logged users or not logged users
-//below code explains the check Authorization token
-// refer https://www.npmjs.com/package/passport-jwt -> An example configuration which reads the JWT from the http Authorization header with the scheme 'bearer'
-
 const JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
 const config = require('./database');
